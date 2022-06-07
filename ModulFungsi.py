0ac80d493f21163af3d4f7c27653e4566b238586
meilindaandarista <meiandarista24@student.uns.ac.id>
2022-06-07 10:04:32 +0700

    mengerjakan team project

---
+++ ModulFungsi.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def checkpp(id):
+    ### mengecheck apakah nomor passporrt yang dimasukkan sama dengan yang ada di list
+    check = False
+    df = pd.read_csv("passpor.csv", delimiter=",")
+    passpor = [list(row) for row in df.values]
+    for i in range(len(passpor)):
+        if passpor[i] == id:
+            check = True
+    return check
+
+def menu():
+    ### menentukkan menu mana yang dipilih user
+    print(""""
+¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+|                           Konversi Mata Uang                          |
+|                                                                       |
+|   1. Daftar Kurs                                                      |
+|   2. Konversi mata uang                                               |
+|   3. Log out                                                          |
+|                                                                       |
+|                                                                       |
+¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+            """)
+    nomor_menu = int(input("Masukkan nomor menu yang diinginkan = "))
+    return nomor_menu
